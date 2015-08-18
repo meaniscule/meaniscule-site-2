92a22b2a8c5c684b387ccd9a27a6f516122fa623
Ash Ryan <ash@ashryanbeats.com>
2015-08-18 14:27:56 -0400

    Basic i18n infra included.

--- client/pre-build/app.js
+++ client/pre-build/app.js
@@ -1,8 +1,10 @@
-var app = angular.module('Meaniscule', ['ui.router']);
+var app = angular.module('Meaniscule', ['ui.router', 'pascalprecht.translate']);
 
-app.config(function ($urlRouterProvider, $locationProvider) {
+app.config(function ($urlRouterProvider, $locationProvider, $translateProvider) {
    // This turns off hashbang urls (/#about) and changes it to something normal (/about)
    $locationProvider.html5Mode(true);
    // If we go to a URL that ui-router doesn't have registered, go to the "/" url.
    $urlRouterProvider.otherwise('/');
+
+   $translateProvider.useSanitizeValueStrategy('escape');
 });
