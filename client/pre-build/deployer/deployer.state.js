e6a5b7c57effa025c89eebc591e681dcf115e3a5
Ash Ryan <ash@ashryanbeats.com>
2015-08-20 19:08:02 -0400

    Deploy menu item string i18n'd.

--- client/pre-build/deployer/deployer.state.js
+++ client/pre-build/deployer/deployer.state.js
@@ -1,24 +1,7 @@
 app.config(function($stateProvider, $translateProvider) {
-    $stateProvider
-        .state('deployer', {
-            url: '/deployer',
-            templateUrl: '/pre-build/deployer/deployer.html',
-            controller: 'DeployerController'
-        })
-        .state('deployer.en', {
-            url: '/en/deployer',
-            templateUrl: '/pre-build/deployer/deployer.html',
-            controller: 'DeployerController',
-            onEnter: function($translate) {
-              $translate.use('en');
-            }
-        })
-        .state('deployer.ja', {
-            url: '/ja/deployer',
-            templateUrl: '/pre-build/deployer/deployer.html',
-            controller: 'DeployerController',
-            onEnter: function($translate) {
-              $translate.use('ja');
-            }
-        });
+    $stateProvider.state('deployer', {
+        url: '/deployer',
+        templateUrl: '/pre-build/deployer/deployer.html',
+        controller: 'DeployerController'
+    });
 });
